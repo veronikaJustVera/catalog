148cea656b45f07ca090ff3700e50ef26eeea782
veronikaJustVera <52103035+veronikaJustVera@users.noreply.github.com>
2019-07-26 18:16:49 +0300

    Update Genre.php

--- models/Genre.php
+++ models/Genre.php
@@ -26,37 +26,7 @@ class Genre
             $categoriesList[$i]['title']=$item['title'];
             $i++;
         }
-        /*switch($catList['title'])
-        {
-            case "modern":
-            $categoriesList['title']='Современная лит-ра';
-            $categoriesList['id']=1; break;
-            case "classic":
-            $categoriesList['title']='Классическая лит-ра';
-            $categoriesList['id']=2; break;
-            case "fantasy":
-            $categoriesList['title']='Фантастика'; 
-            $categoriesList['id']=3; break;
-            case "detective":
-            $categoriesList['title']='Детективы';
-            $categoriesList['id']=4; break;
-            case "mystic":
-            $categoriesList['title']='Мистика';
-            $categoriesList['id']=5; break;
-            case "culinary":
-            $categoriesList['title']='Кулинария';
-            $categoriesList['id']=6; break;
-            case "health":
-            $categoriesList['title']='Здоровье';
-            $categoriesList['id']=7; break;
-            case "business":
-            $categoriesList['title']='Бизнес';
-            $categoriesList['id']=8; break;
-            case "history":
-            $categoriesList['title']='Историческая лит-ра';
-            $categoriesList['id']=9; break;
-
-        }*/
+       
         return $categoriesList;
     }
 }
